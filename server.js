3eecd27745e2cef715dcba92b0668d57323611bf
Ion Nour <ionnouralpha@gmai.com>
2020-11-12 22:24:18 +0200

    ready for deploy

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// process.env.JWT_SECRET = 'e237920e88165bd0090b1c6b544cf7'
+
+const app = express();
+
+(async function conectDB(){
+  try {
+    await mongoose.connect(process.env.MDB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log('MongoDB is connected ...');
+  } catch (err){
+    console.error(err.message);
+    process.exit(1);
+  }
+})();
+
+app.use(express.json({ extended: false }));
+
+//routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/boards', require('./routes/api/boards'));
+app.use('/api/lists', require('./routes/api/lists'));
+app.use('/api/cards', require('./routes/api/cards'));
+app.use('/api/checklists', require('./routes/api/checklists'));
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{console.log("Server runing on port " + PORT);})
